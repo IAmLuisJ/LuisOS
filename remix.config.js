b3c6178cc07bc632e45d82a816c7cd9dd8bc28a0
Luis Juarez <luisisthejuarez@gmail.com>
2022-01-24 21:28:23 -0500

    moving build folder

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,6 @@ module.exports = {
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "build",
+  serverBuildDirectory: "_build",
   ignoredRouteFiles: [".*"]
 };

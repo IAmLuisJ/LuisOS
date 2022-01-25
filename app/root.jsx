19ed367ae50b4e686398539714e06e15e6d5a6f2
Luis Juarez <luisisthejuarez@gmail.com>
2022-01-25 00:38:34 -0500

    removed fallback

--- app/root.jsx
+++ app/root.jsx
@@ -27,17 +27,6 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <span className="text-tertiary absolute flex -translate-y-full transform space-x-1 border-b border-gray-150 bg-white p-2 focus-within:relative focus-within:translate-y-0 dark:border-gray-800 dark:bg-gray-900">
-          <a className="text-primary font-semibold" href="#main">
-            Skip to content
-          </a>
-          <span>(if available)</span>
-          <span>or</span>
-          <a className="text-primary font-semibold" href="#list">
-            jump to list
-          </a>
-          <span>(if available)</span>
-        </span>
         <SiteLayout>
           <Outlet />
         </SiteLayout>

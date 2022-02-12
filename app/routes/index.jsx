ae46ac760e26379dad63a45510979310ec3b0c9a
Luis Juarez <luisisthejuarez@gmail.com>
2022-02-12 10:14:40 -0500

    fixing styles

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Intro from "../components/Intro";
 import ListDetailView from "../components/Layouts/ListDetailView";
+import appStyles from '../styles/app.css';
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: appStyles }]
+}
 
 export default function Index() {
   return (
